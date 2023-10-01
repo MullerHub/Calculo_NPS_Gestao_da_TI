57ec684f7d0ae2c0ce4f05a950e17a95bc1f6f63
Murilo Muller <murilomuller@protonmail.com>
2023-10-01 15:57:10 -0300

    Codigo parece correto, mas estou errando os ponteiros

--- PI.c
+++ PI.c
@@ -4,29 +4,36 @@
 #include <string.h>
 #include <ctype.h>
 #include <locale.h>
+#include <stdbool.h>
+
+#define MAX_CLIENTS 100
 
 // prototipos
-bool fechouNegocio(void);
+bool fechouNegocio();
 double calcularNPS(int notas[], int tamanho);
 
 bool perguntas_e_calculos(const char *question, int *count);
 
-char *printar_na_tela(const char *pergunta, const int *resposta);
+void calcularMetricas(int return_bussiness, int total_interacoes, int up_selling_count, int cross_selling_count, int churn_count);
 
-int num_de_clientes_churn = 0;
-int up_selling_count = 0;
-int cross_selling_count = 0;
-int return_bussiness = 0;
-int churn_count = 0;
-
-int notas_promotores = 0;
-int notas_detratores = 0;
-int notas_neutros = 0;
-int notas_total = 0;
-
+// Main limpa e refatorada!
 int main(int argc, string argv[])
 {
     setlocale(LC_ALL, "");
+    int total_interacoes = 0;
+    int up_selling_count = 0;
+    int cross_selling_count = 0;
+
+    int return_bussiness = 0;
+    char feedbacks[MAX_CLIENTS][100];
+    int num_de_clientes_churn = 0;
+    int churn_count = 0;
+
+    int notas_promotores = 0;
+    int notas_detratores = 0;
+    int notas_neutros = 0;
+    int notas_total = 0;
+
     if (argc < 2)
     {
         printf("Especifique o numero de clientes.\n[Exemplo]: ./PI (quantidade de clientes)");
@@ -34,21 +41,38 @@ int main(int argc, string argv[])
     }
 
     const int numero_de_clientes = atoi(argv[1]);
-    fechouNegocio();
 
+    //
+    while (total_interacoes < numero_de_clientes)
+    {
+        bool fechou = fechouNegocio(&return_bussiness, &total_interacoes, &up_selling_count, &cross_selling_count, &churn_count);
+
+        if (!fechou)
+        {
+            // Cliente não fechou negócio, coletar feedback
+            printf("Que triste que não fechamos negócio, pode nos dar um feedback onde podemos melhorar?\n");
+            char feedback[100];
+            scanf(" %99s", feedback);
+            strcpy(feedbacks[num_de_clientes_churn], feedback);
+            num_de_clientes_churn++;
+        }
+    }
+    calcularMetricas(return_bussiness, total_interacoes, up_selling_count, cross_selling_count, churn_count);
     return 0;
 }
 
-bool fechouNegocio(void)
-{
-    string FechouNegocio;
+// Funções do codigo abaixo!!!
 
+bool fechouNegocio(int *return_bussiness, int *total_interacoes, int *up_selling_count, int *cross_selling_count, int *churn_count)
+{
+    char FechouNegocio;
     do
     {
-        FechouNegocio = get_string("Voce fechou negocio com a gente?? ");
-        if (FechouNegocio[0] == 's' || FechouNegocio[0] == 'S')
+        printf("Você fechou negócio com a gente?? (S/n): ");
+        scanf(" %c", &FechouNegocio);
+
+        if (FechouNegocio == 's' || FechouNegocio == 'S')
         {
-            printf("Voce fechou\n");
             perguntas_e_calculos("Voce fez up-seeling???", &up_selling_count);
             perguntas_e_calculos("Você fez cross-selling???", &cross_selling_count);
             perguntas_e_calculos("Você voltaria a fazer negocios com a gente??", &return_bussiness);
@@ -57,25 +81,22 @@ bool fechouNegocio(void)
             printf("contagem do up_selling %i\n", up_selling_count);
             printf("contagem do cross %i\n", cross_selling_count);
             printf("contagem do return %i\n", return_bussiness);
+            printf("contagem do churn %i\n", churn_count);
 
-            //
-            //
-            //
-            //
-            //
+            (*total_interacoes)++;
             return true;
         }
-        else if (FechouNegocio[0] == 'n' || FechouNegocio[0] == 'N')
+        else if (FechouNegocio == 'N' || FechouNegocio == 'n')
         {
-            printf("Voce nao fechou\n");
+         (*return_bussiness)++;
             return false;
         }
         else
         {
             printf("erro, digite um comando válido [sim ou não]\n");
         }
-    } while ((FechouNegocio[0] != 'N' || FechouNegocio[0] != 'n') ||
-             (FechouNegocio[0] != 's' || FechouNegocio[0] != 'S'));
+    } while (FechouNegocio != 'N' && FechouNegocio != 'n' && FechouNegocio != 'S' && FechouNegocio != 's');
+
     return false;
 }
 
@@ -118,12 +139,32 @@ bool perguntas_e_calculos(const char *question, int *count)
 
     if (resposta == 'S' || resposta == 's')
     {
-        result = true;
-        *count += 1;
+        (*count)++;
+        return true;
     }
     else
     {
         result = false;
     }
-    return result;
 };
+
+void calcularMetricas(int return_bussiness, int total_interacoes, int up_selling_count, int cross_selling_count, int churn_count)
+{
+    // Calcule a porcentagem das pessoas que mantiveram interação após o primeiro contato
+    double porcentagem_manteve_interacao = (double)total_interacoes / numero_de_clientes * 100;
+
+    // Calcule a porcentagem de up-selling e cross-selling entre os que fecharam negócio
+    double porcentagem_fecharam_up_selling = (double)up_selling_count / total_interacoes * 100;
+    double porcentagem_fecharam_cross_selling = (double)cross_selling_count / total_interacoes * 100;
+
+    // Calcule a porcentagem de retorno e churn entre os que fecharam negócio
+    double porcentagem_clientes_retornaram = (double)return_bussiness / total_interacoes * 100;
+    double porcentagem_clientes_churned = (double)churn_count / total_interacoes * 100;
+
+    // Imprima as métricas
+    printf("Porcentagem de pessoas que mantiveram interacao: %.2f%%\n", porcentagem_manteve_interacao);
+    printf("Porcentagem de up-selling entre os que fecharam negócio: %.2f%%\n", porcentagem_fecharam_up_selling);
+    printf("Porcentagem de cross-selling entre os que fecharam negócio: %.2f%%\n", porcentagem_fecharam_cross_selling);
+    printf("Porcentagem de clientes que retornaram: %.2f%%\n", porcentagem_clientes_retornaram);
+    printf("Porcentagem de clientes que entraram em churn: %.2f%%\n", porcentagem_clientes_churned);
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-empty now
-vazio por enquanto
+Para Executar os calculos e testar a regra de negocios, baixe todo o codigo do github ou clone (git clone "")
+
+Após isso, execute o arquivo .exe pelo terminal e adicione o numero de clientes que você queira simular. exemplo: ./PI 3 (quantidade de  três clientes); (./PI é o nome do arquivo executavel)
